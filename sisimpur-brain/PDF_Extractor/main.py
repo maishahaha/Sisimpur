2ae0c2af046ef10c44668ea51a4513ae2800bb60
Arpan Dey <sbxp1966@gmail.com>
2025-05-08 23:09:06 +0600

    Text extraction from pdf enhanced

--- sisimpur-brain/PDF_Extractor/main.py
+++ sisimpur-brain/PDF_Extractor/main.py
@@ -2,19 +2,23 @@ import nltk
 import re
 from pdfminer.high_level import extract_text
 
-pdf_path  = 'sisimpur-brain/PDF_Extractor/data/1mb.pdf'
-raw_text = extract_text(pdf_path)
-print("Extracted text preview:", raw_text[:500])
-# Clean up newlines and spacing
-text = re.sub(r'(?<!\.)\n(?!\n)', ' ', raw_text)
-text = re.sub(r'\s+', ' ', text)
+def extract_ocr_text_based_pdf(pdf_path):
+    text = extract_text(pdf_path)
+    print("Extracted text preview:", text[:500])
+    # Clean up newlines and spacing
+    text = re.sub(r'(?<!\.)\n(?!\n)', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
 
-print("Cleaned text preview:", text[:500])
+    print("Cleaned text preview:", text[:500])
 
 
-#If have any Numbered sentences - Extract numbered sentences
-pattern = re.compile(r'\d+\.\s+.*?(?=\s+\d+\.|$)', re.DOTALL)
-numbered_sentences = pattern.findall(text)
+    # If have any Numbered sentences - Extract numbered sentences
+    pattern = re.compile(r'\d+\.\s+.*?(?=\s+\d+\.|$)', re.DOTALL)
+    numbered_sentences = pattern.findall(text)
 
-for sent in numbered_sentences:
-    print(sent.strip())
+    for sent in numbered_sentences:
+        print(sent.strip())
+
+    return text
+
+extract_ocr_text_based_pdf('sisimpur-brain/PDF_Extractor/data/1mb.pdf')
