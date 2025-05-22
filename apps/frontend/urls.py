2d46d7ba11d9053e53c088702b0fe41a632fc900
Arpan Dey <sbxp1966@gmail.com>
2025-05-22 17:23:33 +0600

    Bug fixed

--- apps/frontend/urls.py
+++ apps/frontend/urls.py
@@ -2,14 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.home, name='home'),
     path('coming-soon/', views.coming_soon, name='coming_soon'),
     path('api/subscribe/', views.subscribe_to_mailchimp, name='subscribe'),
     path('submit/', views.submit_form, name='submit_form'),
-=======
-    path("", views.home, name="home"),
-    path("coming-soon/", views.coming_soon, name="coming_soon"),
-    path("api/subscribe/", views.subscribe_to_mailchimp, name="subscribe"),
->>>>>>> 3783834637fcca91e4f3b3f00f2c1bc310b3f7a2
 ]
