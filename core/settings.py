eaf171c4ea6c2504faf4f7615e074bdaa16df890
Maishahaha <maishaiffatchowdhury@gmail.com>
2025-06-22 00:00:38 +0600

    cleaned up leaderboard

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ CSRF_TRUSTED_ORIGINS = [
 DEBUG = True  # Temporarily enable for debugging
 
 ALLOWED_HOSTS = ["*"]
-WEBSITE_HOST = "defang.dev" # Set the website host [defang.dev, localhost]
+WEBSITE_HOST = "localhost" # Set the website host [defang.dev, localhost]
 
 if WEBSITE_HOST == "defang.dev":
     WEBSITE_URL = "https://arpan8925-web--8000.prod1.defang.dev"
